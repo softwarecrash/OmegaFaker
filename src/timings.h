d6558652644ea308e6a87f688b80b36b95244581
Rocky <44615614+softwarecrash@users.noreply.github.com>
2022-01-18 19:13:39 +0100

    Add files via upload
    added Batterys

--- src/timings.h
+++ src/timings.h
@@ -7,3 +7,25 @@ const int pulseLength[][2] = {
     {5, 105},
 };
 #endif
+#ifdef GreenWorks60V //Timing array for 60V Powerworks - untested from https://endless-sphere.com/forums/viewtopic.php?f=1&t=79384&sid=8676384e4fca92f832bd25d3e6a08a04&start=100#p1559001
+const int pulseLength[][2] = {
+    {1, 412},
+    {3, 205},
+    {9, 105},
+    {1, 205},
+    {4, 105},
+    {4, 205},
+    {3, 105},
+};
+#endif
+#ifdef PowerWorks60V //Timing array for 60V Greenworks - untested from https://endless-sphere.com/forums/viewtopic.php?f=1&t=79384&sid=8676384e4fca92f832bd25d3e6a08a04&start=100#p1658589
+const int pulseLength[][2] = {
+    {1, 412},
+    {3, 205},
+    {8, 105},
+    {2, 205},
+    {3, 105},
+    {4, 205},
+    {3, 105},
+};
+#endif
