c9a3cae519967d25eee267a3c8cfeadf62b8a0f2
Rocky <44615614+softwarecrash@users.noreply.github.com>
2022-01-22 14:15:20 +0100

    Update timings.h
    add 40v raw data

--- src/timings.h
+++ src/timings.h
@@ -7,6 +7,11 @@ const int pulseLength[][2] = {
     {5, 105},
 };
 #endif
+#ifdef Greenworks40VRAW //Timing array for 40V 6Ah (real 3Ah) Aftersale (29462) - read out from battery, working!
+const int pulseLength[][2] = {
+    {1, 412},
+    {24, 105},
+};
 #ifdef GreenWorks60V //Timing array for 60V Powerworks - untested from https://endless-sphere.com/forums/viewtopic.php?f=1&t=79384&sid=8676384e4fca92f832bd25d3e6a08a04&start=100#p1559001
 const int pulseLength[][2] = {
     {1, 412},
